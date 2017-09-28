2dea915e91d5106ecca168be5d43d05a6a5ef69f
dyl25 <dylan-vst@hotmail.com>
2017-09-28 19:40:53 +0200

    début espace validateur

---
+++ app/Http/Controllers/validator/ArticleController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\validator;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class ArticleController extends Controller {
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+
+        $articles = \App\Article::paginate(10);
+
+        //initialisiation de la localisation pour traduire les dates
+        \Carbon\Carbon::setLocale('fr');
+        return view('admin.article.index', compact('articles'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id) {
+        $article = \App\Article::find($id);
+
+        return view('admin.article.show', compact('article'));
+    }
+
+}
